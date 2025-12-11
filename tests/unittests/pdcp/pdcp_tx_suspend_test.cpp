bf53c4de53499342b408f15c9f07beafb048a147
Pedro Alvarez <pedro@srs.io>
2025-12-11 11:40:15 +0000

    pdcp: add log spy to suspend test

--- tests/unittests/pdcp/pdcp_tx_suspend_test.cpp
+++ tests/unittests/pdcp/pdcp_tx_suspend_test.cpp
@@ -8,13 +8,55 @@
  *
  */
 
-#include "pdcp_tx_suspend_test.h"
 #include "pdcp_test_vectors.h"
+#include "pdcp_tx_test_helpers.h"
 #include "ocudu/pdcp/pdcp_config.h"
 #include "ocudu/support/test_utils.h"
 #include <gtest/gtest.h>
 
 using namespace ocudu;
+
+static ocudu::log_sink_spy& test_spy = []() -> ocudu::log_sink_spy& {
+  if (!ocudulog::install_custom_sink(ocudu::log_sink_spy::name(),
+                                     std::make_unique<ocudu::log_sink_spy>(ocudulog::get_default_log_formatter()))) {
+    report_fatal_error("Unable to create logger spy");
+  }
+  auto* spy = static_cast<ocudu::log_sink_spy*>(ocudulog::find_sink(ocudu::log_sink_spy::name()));
+  if (spy == nullptr) {
+    report_fatal_error("Unable to create logger spy");
+  }
+
+  ocudulog::fetch_basic_logger("ROHC", *spy, true);
+  return *spy;
+}();
+
+/// Fixture class for PDCP tests.
+/// It requires TEST_P() and INSTANTIATE_TEST_SUITE_P() to create/spawn tests for each supported SN size.
+class pdcp_tx_suspend_test : public pdcp_tx_test_helper_default_crypto,
+                             public ::testing::Test,
+                             public ::testing::WithParamInterface<std::tuple<pdcp_sn_size, unsigned>>
+{
+protected:
+  void SetUp() override
+  {
+    // Init test's logger.
+    ocudulog::init();
+    logger.set_level(ocudulog::basic_levels::debug);
+
+    // Init RLC logger.
+    ocudulog::fetch_basic_logger("PDCP", false).set_level(ocudulog::basic_levels::debug);
+    ocudulog::fetch_basic_logger("PDCP", false).set_hex_dump_max_size(100);
+
+    // Reset log spy.
+    test_spy.reset_counters();
+  }
+
+  void TearDown() override
+  {
+    // Flush logger after each test.
+    ocudulog::flush();
+  }
+};
 
 /// Test DRB suspend.
 TEST_P(pdcp_tx_suspend_test, when_suspend_called_state_is_reset)
@@ -45,7 +87,7 @@ TEST_P(pdcp_tx_suspend_test, when_suspend_called_state_is_reset)
   assert_pdcp_state_with_reordering(pdcp_tx->get_state(), exp_st);
 
   // Write 5 SDUs again. These should be buffered.
-  // Check for not PDUs in the window and only one resume request.
+  // Check correct number of PDUs in the window and only one resume request.
   for (int i = 0; i < n_pdus; i++) {
     byte_buffer sdu = byte_buffer::create(sdu1).value();
     pdcp_tx->handle_sdu(std::move(sdu));
@@ -59,12 +101,15 @@ TEST_P(pdcp_tx_suspend_test, when_suspend_called_state_is_reset)
 
   FLUSH_AND_ASSERT_EQ(5, pdcp_tx->nof_pdus_in_window());
   FLUSH_AND_ASSERT_EQ(1, test_frame.nof_resume_required);
+
+  EXPECT_EQ(test_spy.get_warning_counter(), 0);
+  EXPECT_EQ(test_spy.get_error_counter(), 0);
 }
 
 ///////////////////////////////////////////////////////////////////
 // Finally, instantiate all testcases for each supported SN size //
 ///////////////////////////////////////////////////////////////////
-std::string test_param_info_to_string(const ::testing::TestParamInfo<std::tuple<pdcp_sn_size, unsigned>>& info)
+static std::string test_param_info_to_string(const ::testing::TestParamInfo<std::tuple<pdcp_sn_size, unsigned>>& info)
 {
   fmt::memory_buffer buffer;
   fmt::format_to(std::back_inserter(buffer), "{}bit", pdcp_sn_size_to_uint(std::get<pdcp_sn_size>(info.param)));

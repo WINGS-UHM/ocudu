376b07431b8c59920dc3159c8c60df9e50b2ad5d
Robert Falkenberg <robert.falkenberg@srs.io>
2025-12-19 17:34:31 +0100

    rohc: add copyright header

--- lib/rohc/rohc_factory.cpp
+++ lib/rohc/rohc_factory.cpp
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright 2021-2025 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
 #include "ocudu/rohc/rohc_factory.h"
 #include "ocudu/rohc/rohc_compressor.h"
 #include "ocudu/rohc/rohc_decompressor.h"
--- lib/rohc/rohc_lib/rohc_lib_decompressor.cpp
+++ lib/rohc/rohc_lib/rohc_lib_decompressor.cpp
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright 2021-2025 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
 #include "rohc_lib_decompressor.h"
 #include "rohc_lib_common.h"
 #include "ocudu/ocudulog/ocudulog.h"

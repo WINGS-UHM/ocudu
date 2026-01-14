661beadcfda7e6ae75018a5d9a12870dd0cf940e
Fabian Eckermann <fabian@srs.io>
2026-01-14 16:34:59 +0100

    cu_cp,rrc: add common ue capabilities type
    Signed-off-by: Fabian Eckermann <fabian@srs.io>

---
+++ include/ocudu/rrc/rrc_ue_capabilities.h
@@ -0,0 +1,19 @@
+/*
+ *
+ * Copyright 2021-2026 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+#pragma once
+
+namespace ocudu::ocucp {
+
+struct rrc_ue_capabilities_t {
+  bool rrc_inactive_supported = false;
+};
+
+} // namespace ocudu::ocucp

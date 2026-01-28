srsran/srsRAN_Project

gtp,f1u: return DL TEID to pool after removal from GTP demux

include/srsran/f1u/du/f1u_gateway.h
@@ -13,6 +13,7 @@
 #include "srsran/f1u/du/f1u_bearer.h"
 #include "srsran/f1u/du/f1u_config.h"
 #include "srsran/f1u/du/f1u_tx_pdu_notifier.h"
+#include "srsran/gtpu/gtpu_teid_pool.h"
 #include "srsran/ran/gnb_du_id.h"
 #include "srsran/ran/qos/five_qi.h"
 #include "srsran/ran/rb_id.h"
@@ -60,6 +61,7 @@ public:
                                                                   five_qi_t                      five_qi,
                                                                   srs_du::f1u_config             config,
                                                                   const gtpu_teid_t&             dl_up_tnl_info,
+                                                                  gtpu_teid_pool&                dl_teid_pool,
                                                                   const up_transport_layer_info& ul_up_tnl_info,
                                                                   srs_du::f1u_du_gateway_bearer_rx_notifier& du_rx,
                                                                   timer_factory                              timers,

include/srsran/f1u/local_connector/f1u_local_connector.h
@@ -15,6 +15,7 @@
 #include "srsran/f1u/cu_up/f1u_gateway.h"
 #include "srsran/f1u/du/f1u_bearer_logger.h"
 #include "srsran/f1u/du/f1u_gateway.h"
+#include "srsran/gtpu/gtpu_teid_pool.h"
 #include "srsran/srslog/srslog.h"
 #include "fmt/std.h"
 #include <cstdint>
@@ -113,23 +114,28 @@ public:
   f1u_gateway_du_bearer(uint32_t                                   ue_index,
                         drb_id_t                                   drb_id,
                         const gtpu_teid_t&                         dl_teid_,
+                        gtpu_teid_pool&                            dl_teid_pool_,
                         srs_du::f1u_du_gateway_bearer_rx_notifier* f1u_rx_,
                         const up_transport_layer_info&             ul_up_tnl_info_,
                         srs_du::f1u_bearer_disconnector&           disconnector_) :
     f1u_rx(f1u_rx_),
     ul_up_tnl_info(ul_up_tnl_info_),
     dl_tnl_info(transport_layer_address::create_from_string(dl_ip_addr), dl_teid_),
+    dl_teid_pool(dl_teid_pool_),
     logger("DU-F1-U", {ue_index, drb_id, dl_tnl_info}),
     disconnector(disconnector_)
   {
   }
 
-  ~f1u_gateway_du_bearer() override { disconnector.remove_du_bearer(dl_tnl_info); }
+  ~f1u_gateway_du_bearer() override { stop(); }
 
   void stop() override
   {
     if (not stopped) {
       disconnector.remove_du_bearer(dl_tnl_info);
+      if (!dl_teid_pool.release_teid(dl_tnl_info.gtp_teid)) {
+        logger.log_warning("Failed to release DL GTP-TEID. teid={}", dl_tnl_info.gtp_teid);
+      }
     }
     stopped = true;
   }
@@ -165,6 +171,7 @@ public:
   const std::string       dl_ip_addr = "127.0.10.2";
   up_transport_layer_info ul_up_tnl_info;
   up_transport_layer_info dl_tnl_info;
+  gtpu_teid_pool&         dl_teid_pool;
 
   srs_du::f1u_bearer_logger logger;
 
@@ -209,6 +216,7 @@ public:
                                                                   five_qi_t                      five_qi,
                                                                   srs_du::f1u_config             config,
                                                                   const gtpu_teid_t&             dl_up_tnl_info,
+                                                                  gtpu_teid_pool&                dl_teid_pool,
                                                                   const up_transport_layer_info& ul_up_tnl_info,
                                                                   srs_du::f1u_du_gateway_bearer_rx_notifier& du_rx,
                                                                   timer_factory                              timers,

WINGS-UHM/ocudu

Signed-off-by: Pedro Alvarez <pedro@srs.io>

include/ocudu/f1u/du/f1u_gateway.h
@@ -13,6 +13,7 @@
 #include "ocudu/f1u/du/f1u_bearer.h"
 #include "ocudu/f1u/du/f1u_config.h"
 #include "ocudu/f1u/du/f1u_tx_pdu_notifier.h"
+#include "ocudu/gtpu/gtpu_teid_pool.h"
 #include "ocudu/ran/gnb_du_id.h"
 #include "ocudu/ran/qos/five_qi.h"
 #include "ocudu/ran/rb_id.h"
@@ -21,7 +22,6 @@
 #include "ocudu/support/timers.h"
 
 namespace ocudu::odu {
-
 /// This class provides a notifier for the RX bearer
 /// inside the DU F1-U gateway. This provides an adapter
 /// to the NR-U bearer to pass SDUs into.
@@ -60,6 +60,7 @@ public:
                                                                   five_qi_t                      five_qi,
                                                                   odu::f1u_config                config,
                                                                   const gtpu_teid_t&             dl_up_tnl_info,
+                                                                  gtpu_teid_pool&                dl_teid_pool,
                                                                   const up_transport_layer_info& ul_up_tnl_info,
                                                                   odu::f1u_du_gateway_bearer_rx_notifier& du_rx,
                                                                   timer_factory                           timers,

include/ocudu/f1u/local_connector/f1u_local_connector.h
@@ -14,6 +14,7 @@
 #include "ocudu/f1u/cu_up/f1u_gateway.h"
 #include "ocudu/f1u/du/f1u_bearer_logger.h"
 #include "ocudu/f1u/du/f1u_gateway.h"
+#include "ocudu/gtpu/gtpu_teid_pool.h"
 #include "ocudu/ocudulog/ocudulog.h"
 #include "fmt/std.h"
 #include <cstdint>
@@ -21,7 +22,6 @@
 #include <unordered_map>
 
 namespace ocudu {
-
 /// \brief Object used to represent a bearer at the CU F1-U gateway
 /// On the co-located case this is done by connecting both entities directly.
 ///
@@ -112,23 +112,28 @@ public:
   f1u_gateway_du_bearer(uint32_t                                ue_index,
                         drb_id_t                                drb_id,
                         const gtpu_teid_t&                      dl_teid_,
+                        gtpu_teid_pool&                         dl_teid_pool_,
                         odu::f1u_du_gateway_bearer_rx_notifier* f1u_rx_,
                         const up_transport_layer_info&          ul_up_tnl_info_,
                         odu::f1u_bearer_disconnector&           disconnector_) :
     f1u_rx(f1u_rx_),
     ul_up_tnl_info(ul_up_tnl_info_),
     dl_tnl_info(transport_layer_address::create_from_string(dl_ip_addr), dl_teid_),
+    dl_teid_pool(dl_teid_pool_),
     logger("DU-F1-U", {ue_index, drb_id, dl_tnl_info}),
     disconnector(disconnector_)
   {
   }
 
-  ~f1u_gateway_du_bearer() override { disconnector.remove_du_bearer(dl_tnl_info); }
+  ~f1u_gateway_du_bearer() override { stop(); }
 
   void stop() override
   {
     if (not stopped) {
       disconnector.remove_du_bearer(dl_tnl_info);
+      if (!dl_teid_pool.release_teid(dl_tnl_info.gtp_teid)) {
+        logger.log_warning("Failed to release DL GTP-TEID. teid={}", dl_tnl_info.gtp_teid);
+      }
     }
     stopped = true;
   }
@@ -164,6 +169,7 @@ public:
   const std::string       dl_ip_addr = "127.0.10.2";
   up_transport_layer_info ul_up_tnl_info;
   up_transport_layer_info dl_tnl_info;
+  gtpu_teid_pool&         dl_teid_pool;
 
   odu::f1u_bearer_logger logger;
 
@@ -208,6 +214,7 @@ public:
                                                                five_qi_t                               five_qi,
                                                                odu::f1u_config                         config,
                                                                const gtpu_teid_t&                      dl_up_tnl_info,
+                                                               gtpu_teid_pool&                         dl_teid_pool,
                                                                const up_transport_layer_info&          ul_up_tnl_info,
                                                                odu::f1u_du_gateway_bearer_rx_notifier& du_rx,
                                                                timer_factory                           timers,
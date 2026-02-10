765a588a1bdc1428e1acf9f64628703bc1238946
Pedro Alvarez <pedro@srs.io>
2026-02-10 12:35:41 +0000

    xnap: check XN setup response ASN.1 message was sent in unit test
    Signed-off-by: Pedro Alvarez <pedro@srs.io>

--- tests/unittests/xnap/xn_setup_procedure_test.cpp
+++ tests/unittests/xnap/xn_setup_procedure_test.cpp
@@ -20,7 +20,7 @@ using namespace ocudu::ocucp;
 class dummy_xnap_message_notifier : public xnap_message_notifier
 {
 public:
-  virtual ~dummy_xnap_message_notifier() = default;
+  ~dummy_xnap_message_notifier() override = default;
 
   bool on_new_message(const xnap_message& msg) override
   {
@@ -43,8 +43,9 @@ protected:
     ocudulog::fetch_basic_logger("XNAP", false).set_level(ocudulog::basic_levels::debug);
     ocudulog::fetch_basic_logger("XNAP", false).set_hex_dump_max_size(100);
 
-    std::unique_ptr<xnap_message_notifier> assoc = std::make_unique<dummy_xnap_message_notifier>();
-    xnap                                         = std::make_unique<xnap_impl>(xnap_local_cfg, ctrl_worker);
+    auto assoc = std::make_unique<dummy_xnap_message_notifier>();
+    xnap       = std::make_unique<xnap_impl>(xnap_local_cfg, ctrl_worker);
+    tx_assoc   = assoc.get();
     xnap->set_tx_association_notifier(std::move(assoc));
   }
 
@@ -52,6 +53,7 @@ protected:
   {
     // Flush logger after each test.
     ocudulog::flush();
+    tx_assoc = nullptr;
   }
 
   ocudulog::basic_logger&    logger = ocudulog::fetch_basic_logger("TEST", false);
@@ -78,12 +80,30 @@ protected:
   xnap_configuration xnap_peer_cfg = {
       peer_gnb_id,
       std::vector<supported_tracking_area>{{peer_tac, std::vector<plmn_item>{{peer_plmn, peer_slice_support_list}}}}};
+
+  std::optional<xnap_message> get_last_message()
+  {
+    if (tx_assoc == nullptr) {
+      return std::nullopt;
+    }
+    return tx_assoc->last_msg;
+  }
+
+private:
+  dummy_xnap_message_notifier* tx_assoc;
 };
 
 TEST_F(xn_setup_procedure_test, when_correct_setup_received_from_peer_setup_complete_is_sent)
 {
   xnap_message xn_setup_req = generate_xn_setup_request(xnap_peer_cfg);
   xnap->handle_message(xn_setup_req);
+  std::optional<xnap_message> rep = get_last_message();
+
+  // Check XN setup response.
+  ASSERT_TRUE(rep.has_value());
+  ASSERT_EQ(rep->pdu.type(), asn1::xnap::xn_ap_pdu_c::types_opts::successful_outcome);
+  ASSERT_EQ(rep->pdu.successful_outcome().value.type(),
+            asn1::xnap::xnap_elem_procs_o::successful_outcome_c::types_opts::xn_setup_resp);
 }
 
 int main(int argc, char** argv)

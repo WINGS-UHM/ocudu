3b551c71740a5bb584cff0d925c28449850fa15b
Robert Falkenberg <robert.falkenberg@srs.io>
2026-02-27 11:51:34 +0100

    license: update license headers
    Signed-off-by: Robert Falkenberg <robert.falkenberg@srs.io>

--- include/ocudu/adt/stable_id_map.h
+++ include/ocudu/adt/stable_id_map.h
@@ -1,12 +1,6 @@
-/*
- *
- * Copyright 2021-2026 Software Radio Systems Limited
- *
- * By using this file, you agree to the terms and conditions set
- * forth in the LICENSE file which can be found at the top level of
- * the distribution.
- *
- */
+// Copyright 2021-2026 Software Radio Systems Limited
+// SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+// SPDX-License-Identifier: BSD-3-Clause-Open-MPI
 
 #pragma once
 

c8206676e53e6ab1264b036d919bbac256e01357
nexuseli18 <sparkar32@gmail.com>
2026-02-27 15:38:53 -1000

    [updates] modifs to dashboard

--- docker/telegraf/ws_adapter.py
+++ docker/telegraf/ws_adapter.py
@@ -15,7 +15,8 @@ def _on_message(_ws: websocket.WebSocketApp, message: str):
     with suppress(json.JSONDecodeError):
         metric = json.loads(message)
         if "cmd" not in metric:
-            print(json.dumps(metric))
+            print("-" * 60)
+            print(json.dumps(metric, indent=2))
 
 
 if __name__ == "__main__":

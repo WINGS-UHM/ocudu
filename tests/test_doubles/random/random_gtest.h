bc9dbdd27828d3ab0911b3b2fafe591cc30a99c2
Francisco Paisana <francisco.paisana@srs.io>
2026-03-03 19:40:16 +0100

    sched: implementation of random generator for tests
    Signed-off-by: Francisco Paisana <francisco.paisana@srs.io>

---
+++ tests/test_doubles/random/random_gtest.h
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+// SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+// Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
+
+#include <gtest/gtest.h>
+#include <random>
+#include <vector>
+
+namespace ocudu::test_random {
+
+/// Fetch the seed under use for unit tests. The user can force a seed via --gtest_random_seed= command line option.
+uint32_t seed();
+
+/// Fetch thread-local random generator.
+std::mt19937& tls_gen();
+
+/// Returns a random integer with uniform distribution within the specified bounds.
+template <typename Integer>
+Integer uniform_int(Integer min, Integer max)
+{
+  return std::uniform_int_distribution<Integer>{min, max}(tls_gen());
+}
+template <typename Integer>
+Integer uniform_int()
+{
+  return uniform_int(std::numeric_limits<Integer>::min(), std::numeric_limits<Integer>::max());
+}
+
+/// Return a vector of integers with specified size filled with random values.
+template <typename Integer>
+std::vector<Integer> random_vector(size_t sz)
+{
+  std::vector<Integer> vec(sz);
+  for (unsigned i = 0; i != sz; ++i) {
+    vec[i] = uniform_int<Integer>();
+  }
+  return vec;
+}
+
+} // namespace ocudu::test_random
---
+++ tests/test_doubles/random/random_gtest_main.cpp
@@ -0,0 +1,82 @@
+// SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+// SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+// Portions of this file may implement 3GPP specifications, which may be subject to additional licensing requirements.
+
+#include "random_gtest.h"
+#include "ocudu/ocudulog/ocudulog.h"
+
+/// Global base random seed that can be either set via command line (--gtest_random_seed=) or automatically generated.
+static uint32_t base_seed = 0;
+
+/// Global counter of tests run.
+static std::atomic<uint32_t> test_counter{0};
+
+namespace {
+
+/// Gtest environment for setting up random seeds.
+class RandomSeedEnvironment : public ::testing::Environment
+{
+public:
+  RandomSeedEnvironment()
+  {
+    base_seed = ::testing::UnitTest::GetInstance()->random_seed();
+    if (base_seed == 0) {
+      // When seed == 0, it means that the user did not explicitly set a seed (according to gtest). We generate one.
+      base_seed = std::random_device{}();
+    }
+    fmt::print("[TEST] --- RANDOM SEED: {} ---\n", base_seed);
+  }
+};
+
+class RandomGeneratorResetListener final : public ::testing::EmptyTestEventListener
+{
+private:
+  void OnTestStart(const testing::TestInfo& test_info) override
+  {
+    // On each test start, we bump the test counter.
+    test_counter.fetch_add(1, std::memory_order_acq_rel);
+  }
+
+  void OnTestEnd(const testing::TestInfo& test_info) override
+  {
+    ocudulog::flush();
+    if (test_info.result()->Failed()) {
+      fmt::print(stderr, "[  FAILED  ] Seed: {}\n", ocudu::test_random::seed());
+    }
+  }
+};
+
+} // namespace
+
+uint32_t ocudu::test_random::seed()
+{
+  return base_seed;
+}
+
+std::mt19937& ocudu::test_random::tls_gen()
+{
+  thread_local std::mt19937 rng(seed());
+  thread_local uint32_t     last_test_counter{0};
+
+  uint32_t cur_test_counter = test_counter.load(std::memory_order_acquire);
+  if (cur_test_counter != last_test_counter) {
+    // New test started. Reset generator.
+    last_test_counter = cur_test_counter;
+    rng.seed(cur_test_counter);
+  }
+  return rng;
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  // Set up global seed.
+  ::testing::AddGlobalTestEnvironment(new RandomSeedEnvironment{});
+
+  // Setup test random listener.
+  auto& listeners = ::testing::UnitTest::GetInstance()->listeners();
+  listeners.Append(new RandomGeneratorResetListener{});
+
+  return RUN_ALL_TESTS();
+}

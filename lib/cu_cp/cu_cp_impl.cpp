e58b6d2bda9c97a9a894960d36cb20958ca95b5a
Guillaume Belanger <guillaume.belanger27@gmail.com>
2026-04-21 10:34:36 -0400

    fix: use launch_no_ip_task
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- lib/cu_cp/cu_cp_impl.cpp
+++ lib/cu_cp/cu_cp_impl.cpp
@@ -912,18 +912,11 @@ cu_cp_impl::handle_new_pdu_session_resource_release_command(const cu_cp_pdu_sess
   cu_cp_ue* ue = ue_mng.find_du_ue(command.ue_index);
   if (ue == nullptr) {
     logger.error("ue={}: Dropping PDUSessionResourceReleaseCommand. Could not find DU UE", command.ue_index);
-    return launch_async([](coro_context<async_task<cu_cp_pdu_session_resource_release_response>>& ctx) {
-      CORO_BEGIN(ctx);
-      CORO_RETURN(cu_cp_pdu_session_resource_release_response{});
-    });
+    return launch_no_op_task(cu_cp_pdu_session_resource_release_response{});
   }
   if (ue->get_cu_up_index() == cu_up_index_t::invalid) {
-    logger.error("ue={}: Dropping PDUSessionResourceReleaseCommand. Could not find CU-UP of the UE",
-                   command.ue_index);
-    return launch_async([](coro_context<async_task<cu_cp_pdu_session_resource_release_response>>& ctx) {
-      CORO_BEGIN(ctx);
-      CORO_RETURN(cu_cp_pdu_session_resource_release_response{});
-    });
+    logger.error("ue={}: Dropping PDUSessionResourceReleaseCommand. Could not find CU-UP of the UE", command.ue_index);
+    return launch_no_op_task(cu_cp_pdu_session_resource_release_response{});
   }
 
   return launch_async<pdu_session_resource_release_routine>(
